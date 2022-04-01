695202e1779315386803974acc3e7cb244303b60
adebiyitokosi <adebiyitokosi@gmail.com>
2022-04-01 12:01:21 -0400

    configuring your ec2 with the security group my-sg

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids      = [aws_security_group.my-sg.id]
   tags = {
     Name = "HelloWorld"
   }
